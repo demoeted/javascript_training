f15c8b6e2049a8c05ac12202eb9875aa1ec6db98
Almar Prado <almarsamprado1@gmail.com>
2024-05-02 09:24:45 -0500

    Started Javascript Training - Syntax and Types

---
+++ chapter_2/sandbox.js
@@ -0,0 +1,46 @@
+// - - Intro - -
+// alert('hello, world');
+
+// console.log(1);
+// console.log(2);
+
+// - - Variables - - 
+// let age = 25; 
+// let year = 2019;
+
+// console.log(age, year)
+
+// age = 30;
+// console.log(age);
+
+// const points = 100; 
+// // points = 50; This will create an error since points is a constant and its value cannot change.
+// console.log(points);
+
+// var score = 75;
+// console.log(score);
+
+
+// - - Strings - - 
+console.log('hello, world');
+
+let email = 'mario@thenetninja.co.uk';
+console.log(email);
+
+ // string concatenation
+let firstName ='Brandon';
+let lastName = 'Sanderson';
+
+let fullName = firstName + ' ' + lastName;
+console.log(fullName);
+
+// Retrieving characters from string
+console.log(fullName[0]);
+console.log(fullName[2]);
+
+// String length property
+console.log(fullName.length);
+
+// String methods
+console.log(fullName.toUpperCase());
+let result = fullName.toLowerCase();
