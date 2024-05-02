aac306ac205aa50b9ee80190ff171e627447d4cf
Almar Prado <almarsamprado1@gmail.com>
2024-05-02 11:25:52 -0500

    Finished learning loops with Javascript.

---
+++ chapter_3/sandbox.js
@@ -0,0 +1,40 @@
+// - -  Loops - -
+
+// For Loops
+
+// for(let i = 0; i < 5; i++){
+//     console.log('In loop: ', i)
+// }
+// console.log('Loop finished')
+
+// const names = ['shaun','mario','luigi']
+
+// for(let i = 0; i < names.length; i++){
+//     // console.log(names[i], i)
+//     let html = `<div>${names[i]}</div>`
+//     console.log(html)
+// }
+
+// While Loops
+
+// const names = ['shaun','mario','luigi']
+
+// let i = 0
+// while(i < 5){
+//     console.log('in loop;', i)
+//     i++
+// }
+
+// let i = 0
+// while (i < names.length){
+//     console.log(names[i])
+//     i++
+// }
+
+// Do while loops
+
+let i = 5
+do {
+    console.log('Value of i is: ', i)
+    i++
+} while(i < 5)
