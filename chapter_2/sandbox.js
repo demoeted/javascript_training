8253ce214b83dbfcd77a01af13c30267cc16eb39
Almar Prado <almarsamprado1@gmail.com>
2024-05-02 09:41:18 -0500

    Finished Learning String data type with Javascript.

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,46 +1,65 @@
 // - - Intro - -
-// alert('hello, world');
+// alert('hello, world')
 
-// console.log(1);
-// console.log(2);
+// console.log(1)
+// console.log(2)
 
 // - - Variables - - 
-// let age = 25; 
-// let year = 2019;
+// let age = 25 
+// let year = 2019
 
 // console.log(age, year)
 
-// age = 30;
-// console.log(age);
+// age = 30
+// console.log(age)
 
-// const points = 100; 
-// // points = 50; This will create an error since points is a constant and its value cannot change.
-// console.log(points);
+// const points = 100 
+// // points = 50 This will create an error since points is a constant and its value cannot change.
+// console.log(points)
 
-// var score = 75;
-// console.log(score);
+// var score = 75
+// console.log(score)
 
 
 // - - Strings - - 
-console.log('hello, world');
+// console.log('hello, world')
 
-let email = 'mario@thenetninja.co.uk';
-console.log(email);
+// let email = 'mario@thenetninja.co.uk'
+// console.log(email)
 
- // string concatenation
-let firstName ='Brandon';
-let lastName = 'Sanderson';
+// // string concatenation
+// let firstName ='Brandon'
+// let lastName = 'Sanderson'
 
-let fullName = firstName + ' ' + lastName;
-console.log(fullName);
+// let fullName = firstName + ' ' + lastName
+// console.log(fullName)
 
-// Retrieving characters from string
-console.log(fullName[0]);
-console.log(fullName[2]);
+// // Retrieving characters from string
+// console.log(fullName[0])
+// console.log(fullName[2])
 
-// String length property
-console.log(fullName.length);
+// // String length property
+// console.log(fullName.length)
 
-// String methods
-console.log(fullName.toUpperCase());
-let result = fullName.toLowerCase();
+// // String methods
+// console.log(fullName.toUpperCase())
+// let result = fullName.toLowerCase()
+// console.log(result, fullName)
+
+// let index = email.indexOf('@')
+// console.log(index)
+
+// Common String methods
+// let email = 'mario@thenetninja.co.uk'
+
+// let result = email.lastIndexOf('n')
+
+// let result = email.slice(0,10) // (start index, end index)
+
+// let result = email.substr(4,10) // (start index, character length)
+
+// let result = email.replace('m','w') // (find character, replacement)
+
+// let result = email.replace('n','w') // Replaces first instance of the find character
+
+// console.log(result)
